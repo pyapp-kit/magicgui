2092306551c750c1e1eb2171e311512bf61f5da8
Talley Lambert <talley.lambert@gmail.com>
2020-02-06 23:23:35 -0500

    trying to fix versioning

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tlambert03/magicgui",
-    version="version='0.0.3'",
+    version='0.0.2',
     zip_safe=False,
 )
