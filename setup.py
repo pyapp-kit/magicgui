81783f639d16cbd6cebe0105039987437c8bc171
Talley Lambert <talley.lambert@gmail.com>
2020-02-06 23:14:41 -0500

    remove py35

--- setup.py
+++ setup.py
@@ -20,14 +20,13 @@ test_requirements = [
 setup(
     author="Talley Lambert",
     author_email="talley.lambert@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
