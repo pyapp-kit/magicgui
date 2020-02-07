942d03edad21336669092a7be4b4457320478f40
Talley Lambert <talley.lambert@gmail.com>
2020-02-06 23:11:44 -0500

    Bump version: 0.0.0 → 0.0.1

--- magicgui/__init__.py
+++ magicgui/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Talley Lambert"""
 __email__ = "talley.lambert@gmail.com"
-__version__ = "0.0.0"
+__version__ = "__version__ = '0.0.1'"
 
 from .magicgui import magicgui
 
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tlambert03/magicgui",
-    version="0.0.0",
+    version="version='0.0.1'",
     zip_safe=False,
 )
