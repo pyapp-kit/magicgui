50cbb0184db964f6ecc34cf18d9e252418b393af
Talley Lambert <talley.lambert@gmail.com>
2020-02-09 08:47:33 -0500

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tlambert03/magicgui",
-    version="0.0.6",
+    version="0.0.7",
     zip_safe=False,
 )
