52ed225ed853c02b57b3490450abe25db6c9be80
Talley Lambert <talley.lambert@gmail.com>
2020-02-18 20:14:13 -0500

    trim and fix setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open("README.md", encoding='utf-8') as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-requirements = []
-
-setup_requirements = [
-    "pytest-runner",
-]
-
-test_requirements = [
-    "pytest>=3",
-]
 
 setup(
+    name="magicgui",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["qtpy>=1.7.0"],
+    python_requires=">=3.6",
     author="Talley Lambert",
     author_email="talley.lambert@gmail.com",
-    python_requires=">=3.6",
+    description="build GUIs from functions, using magic.",
+    url="https://github.com/tlambert03/magicgui",
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords="magicgui",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -33,19 +34,5 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Visualization",
     ],
-    description="build GUIs from functions, using magic.",
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    include_package_data=True,
-    keywords="magicgui",
-    name="magicgui",
-    packages=find_packages(include=["magicgui", "magicgui.*"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/tlambert03/magicgui",
-    version="0.1.0",
-    zip_safe=False,
+    extras_require={"PySide2": ["PySide2>=5.12.0"], "PyQt5": ["PyQt5>=5.12.0"]},
 )
