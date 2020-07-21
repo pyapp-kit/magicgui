717f87b14c6e6e2d68af2b70756106235c8b943f
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-07-21 11:15:59 +1000

    Add a logarithmic scale slider class

---
+++ magicgui/_qt/widgets/log_slider.py
@@ -0,0 +1,25 @@
+"""A Slider with a natural logarithmic scale."""
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QSlider
+
+
+class QLogSlider(QSlider):
+    """A Slider Widget with a natural logarithmic scale."""
+
+    PRECISION = 1000
+
+    def __init__(self, parent=None):
+        super().__init__(Qt.Horizontal, parent=parent)
+        self.setMaximum(10)
+
+    def value(self):
+        """Get the natural log slider value as a float."""
+        return np.log(super().value() / self.PRECISION)
+
+    def setValue(self, value):
+        """Set integer slier position from float ``value``."""
+        super().setValue(np.exp(value) * self.PRECISION)
+
+    def setMaximum(self, value):
+        """Set maximum position of slider in float units."""
+        super().setMaximum(np.exp(value) * self.PRECISION)
