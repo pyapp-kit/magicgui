9e69e4194200f141446c99dc661946d0cee698f4
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-07-21 12:38:17 +1000

    Add basic tests for QLogSlider and QDoubleSlider classes

---
+++ magicgui/_tests/test_widgets.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+
+from magicgui._qt.widgets import QDoubleSlider, QLogSlider
+
+
+@pytest.mark.parametrize("SliderClass", [QDoubleSlider, QLogSlider])
+def test_slider(qtbot, SliderClass):
+    slider = SliderClass()
+    qtbot.addWidget(slider)
+    assert slider.value() == 0  # default slider value is zero
+    slider.setValue(np.pi)
+    assert np.isclose(slider.value(), np.pi, atol=0.01)
+    slider.setMaximum(10)
+    slider.setValue(11)  # above maximum allowed value, value will be clipped
+    assert np.isclose(slider.value(), 10)
