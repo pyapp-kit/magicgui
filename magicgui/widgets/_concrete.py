8740c1ee16700bd45f467ca90eef9fb873a09644
Talley Lambert <talley.lambert@gmail.com>
2021-01-05 08:33:25 -0500

    coerce maximum/minimum to max/min

--- magicgui/widgets/_concrete.py
+++ magicgui/widgets/_concrete.py
@@ -194,6 +194,19 @@ class LogSlider(TransformedRangedWidget):
     def __init__(
         self, min: float = 1, max: float = 100, base: float = math.e, **kwargs
     ):
+        for key in ("maximum", "minimum"):
+            if key in kwargs:
+                import warnings
+
+                warnings.warn(
+                    f"The {key!r} keyword arguments has been changed to {key[:3]!r}. "
+                    "In the future this will raise an exception\n",
+                    FutureWarning,
+                )
+                if key == "maximum":
+                    max = kwargs.pop(key)
+                else:
+                    min = kwargs.pop(key)
         self._base = base
         app = use_app()
         assert app.native
