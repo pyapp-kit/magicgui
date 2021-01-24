0a6961e75365a4ee5809d4e2e56e30a0b9503589
Talley Lambert <talley.lambert@gmail.com>
2021-01-24 17:45:25 -0500

    Improve fallback behavior of tqdm iterator inside of *hidden* magicgui widget (#131)

--- tests/test_tqdm.py
+++ tests/test_tqdm.py
@@ -47,6 +47,7 @@ def test_no_leave_tqdm():
             pass
         assert pbar2.progressbar.visible is False
 
+    f2.show()
     f2()
 
 
@@ -60,7 +61,44 @@ def test_unbound_tqdm():
             # undefined range will render as a "busy" indicator
             assert pbar.progressbar.range == (0, 0)
 
+    f.show()
     f()
+
+
+def test_tqdm_std_err(capsys):
+    """Test that tqdm inside an invisible magicgui falls back to console behavior."""
+
+    # outside of a magicgui it falls back to tqdm_std behavior
+    with tqdm(range(10)) as t_obj:
+        iter(t_obj)
+    captured = capsys.readouterr()
+
+    assert "0%|" in str(captured.err)
+    assert "| 0/10" in str(captured.err)
+    assert not str(captured.out)
+
+
+def test_tqdm_in_visible_mgui_std_err(capsys):
+    """Test that tqdm inside an mgui outputs to console only when mgui not visible."""
+
+    # inside of a of a magicgui it falls back to tqdm_std behavior
+    @magicgui
+    def f():
+        with tqdm(range(10)) as t_obj:
+            iter(t_obj)
+
+    f()
+    captured = capsys.readouterr()
+    assert "0%|" in str(captured.err)
+    assert "| 0/10" in str(captured.err)
+    assert not str(captured.out)
+
+    # showing the widget disables the output to console behavior
+    f.show()
+    f()
+    captured = capsys.readouterr()
+    assert not str(captured.err)
+    assert not str(captured.out)
 
 
 # Test various ways that tqdm might need to traverse the frame stack:
@@ -75,6 +113,7 @@ def test_tqdm_inside_of_magicgui():
             sleep(0.02)
 
     # before calling the function, we won't have any progress bars
+    long_func.show()
     assert not long_func._tqdm_pbars
     long_func()
     # after calling the it, we should now have a progress bars
@@ -92,22 +131,10 @@ def test_trange_inside_of_magicgui():
         for i in trange(4):
             pass
 
+    long_func.show()
     assert not long_func._tqdm_pbars
     long_func()
     assert len(long_func._tqdm_pbars) == 1
-
-
-@magicgui
-def directly_decorated(steps=2):
-    for i in trange(4):
-        pass
-
-
-def test_trange_inside_of_global_magicgui():
-    """Test that trange can find the magicgui within which it is called."""
-    assert not directly_decorated._tqdm_pbars
-    directly_decorated()
-    assert len(directly_decorated._tqdm_pbars) == 1
 
 
 def _indirectly_decorated(steps=2):
@@ -115,11 +142,10 @@ def _indirectly_decorated(steps=2):
         pass
 
 
-indirectly_decorated = magicgui(_indirectly_decorated)
-
-
 def test_trange_inside_of_indirectly_decorated_magicgui():
     """Test that trange can find the magicgui within which it is called."""
+    indirectly_decorated = magicgui(_indirectly_decorated)
+    indirectly_decorated.show()
     assert not indirectly_decorated._tqdm_pbars
     indirectly_decorated()
     assert len(indirectly_decorated._tqdm_pbars) == 1
@@ -134,6 +160,7 @@ def test_tqdm_nested():
             for x in trange(4):
                 pass
 
+    long_func.show()
     # before calling the function, we won't have any progress bars
     assert not long_func._tqdm_pbars
     long_func()
@@ -150,6 +177,7 @@ def test_tqdm_nested():
         for x in trange(4):
             pass
 
+    long_func2.show()
     # before calling the function, we won't have any progress bars
     assert not long_func2._tqdm_pbars
     long_func2()
