095c084690d07030ba9960a95409451ced8ce011
Talley Lambert <talley.lambert@gmail.com>
2022-01-01 11:56:42 -0500

    improve test

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -715,13 +715,12 @@ def test_slice_edit_events():
     assert sl.value == slice(start, stop, step)
 
 
-def test_pushbutton():
-    from unittest.mock import MagicMock
-
-    from magicgui.widgets import PushButton
-
-    btn = PushButton(text="click me")
+def test_pushbutton_cick_signal():
+    btn = widgets.PushButton(text="click me")
     mock = MagicMock()
+    mock2 = MagicMock()
     btn.changed.connect(mock)
+    btn.clicked.connect(mock2)
     btn.native.click()
     mock.assert_called_once()
+    mock2.assert_called_once()
