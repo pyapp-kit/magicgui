01e6d07964a60fa74166690dc68a3538d8f4ea37
Talley Lambert <talley.lambert@gmail.com>
2022-05-02 14:39:32 -0400

    fix typing

--- magicgui/widgets/_bases/ranged_widget.py
+++ magicgui/widgets/_bases/ranged_widget.py
@@ -1,26 +1,24 @@
+import builtins
 from abc import ABC, abstractmethod
 from math import ceil, log10
-from typing import Tuple
+from typing import Tuple, Union, cast
 from warnings import warn
 
 from magicgui.widgets import _protocols
 
-from .value_widget import UNSET, ValueWidget
-
-py_min = min
-py_max = max
-# Workaround for names that are used in RangeWidget constructor
+from .value_widget import UNSET, ValueWidget, _Unset
 
 
 class RangedWidget(ValueWidget):
-    """Widget with a contstrained value. Wraps RangedWidgetProtocol.
+    """Widget with a constrained value. Wraps RangedWidgetProtocol.
 
     Parameters
     ----------
     min : float, optional
-        The minimum allowable value, by default 0
+        The minimum allowable value, by default 0 (or `value` if `value` is less than 0)
     max : float, optional
-        The maximum allowable value, by default 1000
+        The maximum allowable value, by default 1000 (or `value` if `value` is greater
+        than 1000)
     step : float, optional
         The step size for incrementing the value, by default 1
     """
@@ -28,8 +26,12 @@ class RangedWidget(ValueWidget):
     _widget: _protocols.RangedWidgetProtocol
 
     def __init__(
-        self, min: float = UNSET, max: float = UNSET, step: float = 1, **kwargs
-    ):
+        self,
+        min: Union[float, _Unset] = UNSET,
+        max: Union[float, _Unset] = UNSET,
+        step: float = 1,
+        **kwargs,
+    ):  # sourcery skip: avoid-builtin-shadow
         for key in ("maximum", "minimum"):
             if key in kwargs:
                 warn(
@@ -48,11 +50,13 @@ class RangedWidget(ValueWidget):
         tmp_val = float(val if val not in (UNSET, None) else 1)
 
         self.step = step
-        self.min = min if min is not UNSET else py_min(0, tmp_val)
-        self.max = (
-            max
+        self.min: float = (
+            cast(float, min) if min is not UNSET else builtins.min(0, tmp_val)
+        )
+        self.max: float = (
+            cast(float, max)
             if max is not UNSET
-            else py_max(1000, 10 ** ceil(log10(py_max(1, tmp_val + 1)))) - 1
+            else builtins.max(1000, 10 ** ceil(log10(builtins.max(1, tmp_val + 1)))) - 1
         )
         if val not in (UNSET, None):
             self.value = val
