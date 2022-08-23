aa8d28d809ac3bddb78764c656c55a24e31933c2
Talley Lambert <talley.lambert@gmail.com>
2022-08-23 18:37:20 -0400

    add more fields

--- tests/test_guimodel.py
+++ tests/test_guimodel.py
@@ -1,22 +1,32 @@
 """For now, this module assumes qt backend.
 """
+import ipaddress
+import uuid
 from datetime import date
+from enum import Enum
+from typing import List, Optional
 
 from magicgui import Field, GUIModel
 from magicgui.widgets import Container
 
 
 def test_basic_model(qapp):
-    class MyModel(GUIModel):
+    class Degree(Enum):
+        BACHELOR = "Bachelor"
+        MASTER = "Master"
+        PHD = "PhD"
+
+    class Person(GUIModel):
         name: str
         age: int
-        married: bool
-        salary: float = Field(..., ge=0, le=999999)
         birthday: date
+        married: bool = False
+        salary: Optional[float] = Field(None, ge=0, le=999999)
+        children: List[ipaddress.IPv4Address] = []
+        uid: uuid.UUID = Field(default_factory=uuid.uuid4)
+        degree: Degree = Degree.BACHELOR
 
-    obj = MyModel(
-        name="Frank", age=55, married=True, salary=100000, birthday=date(1980, 1, 1)
-    )
+    obj = Person(name="Frank", age=55, married=True, birthday=date(1965, 1, 1))
     assert obj._gui is None
     assert isinstance(obj.gui, Container)
     assert obj._gui is not None
