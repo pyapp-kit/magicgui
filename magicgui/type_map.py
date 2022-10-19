a284a01108aabdc49840f9de1324a70032583855
Hanjin Liu <40591297+hanjinliu@users.noreply.github.com>
2022-10-20 08:06:57 +0900

    Support `Literal` type (#458)
    * support Literal
    
    * support set[Literal[...]] and add test
    
    * add ipynb Select widget
    
    * Update tests/test_widgets.py
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- magicgui/type_map.py
+++ magicgui/type_map.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 from enum import EnumMeta
 from typing import Any, Callable, DefaultDict, ForwardRef, Type, TypeVar, cast, overload
 
-from typing_extensions import Literal
+from typing_extensions import Literal, get_origin
 
 from magicgui import widgets
 from magicgui.types import (
@@ -83,14 +83,23 @@ def match_type(tw: TypeWrapper) -> WidgetTuple | None:
     if _type in (types.FunctionType,):
         return widgets.FunctionGui, {"function": tw.default}  # type: ignore
 
-    # sequence of paths
     if tw.shape in tw.SHAPE.SEQUENCE_LIKE:
         if _is_subclass(tw.type_, pathlib.Path):
+            # sequence of paths
             return widgets.FileEdit, {"mode": "rm"}
         elif tw.shape == tw.SHAPE.LIST:
             return widgets.ListEdit, {}
         elif tw.shape == tw.SHAPE.TUPLE:
             return widgets.TupleEdit, {}
+
+    if tw.shape == tw.SHAPE.SET:
+        if len(_type.__args__) > 0:
+            arg = _type.__args__[0]
+            if get_origin(arg) is Literal:
+                return widgets.Select, {"choices": arg.__args__}
+
+    if get_origin(_type) is Literal:
+        return widgets.ComboBox, {"choices": _type.__args__}
     return None
 
 
