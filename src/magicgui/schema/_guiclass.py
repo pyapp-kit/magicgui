90f30e2ca42ce0308463c9116a4cece2bc5bbaff
Talley Lambert <talley.lambert@gmail.com>
2022-11-26 13:40:17 -0500

    feat: add guiclass decorator (#498)
    * feat: add guiclass decorator
    
    * add more kwargs
    
    * rearrange
    
    * refactor: move into schema
    
    * fix import
    
    * test: fix import
    
    * test: changes to docs, and tests for frozen and slots
    
    * fix: remove version
    
    * feat: more typing
    
    * add __dataclass_transform__
    
    * add exports
    
    * update fieldspecifiers
    
    * add class-based approach too

---
+++ src/magicgui/schema/_guiclass.py
@@ -0,0 +1,335 @@
+"""`guiclass` decorator.
+
+1. Turns a class into a dataclass
+2. Uses `psygnal.evented` to make it an [evented
+   dataclass](https://psygnal.readthedocs.io/en/latest/dataclasses/)
+2. Adds a `gui` property to the class that will return a `magicgui` widget, bound to
+the values of the dataclass instance.
+"""
+from __future__ import annotations
+
+import contextlib
+import warnings
+from dataclasses import Field, dataclass, field, is_dataclass
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    ClassVar,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
+
+from psygnal import SignalGroup, evented
+
+from magicgui.schema._ui_field import build_widget
+from magicgui.widgets import PushButton
+from magicgui.widgets.bases import ContainerWidget, ValueWidget
+
+if TYPE_CHECKING:
+    from typing import Protocol
+
+    from typing_extensions import TypeGuard
+
+    # fmt: off
+    class GuiClassProtocol(Protocol):
+        """Protocol for a guiclass."""
+
+        @property
+        def gui(self) -> ContainerWidget: ...
+        @property
+        def events(self) -> SignalGroup: ...
+    # fmt: on
+
+
+__all__ = ["guiclass", "button", "is_guiclass", "unbind_gui_from_instance"]
+_BUTTON_ATTR = "_magicgui_button"
+_GUICLASS_FLAG = "__magicgui_guiclass__"
+
+_T = TypeVar("_T")
+T = TypeVar("T", bound=Type[Any])
+F = TypeVar("F", bound=Callable)
+
+
+# https://github.com/microsoft/pyright/blob/main/specs/dataclass_transforms.md
+def __dataclass_transform__(
+    *,
+    eq_default: bool = True,
+    order_default: bool = False,
+    kw_only_default: bool = False,
+    field_specifiers: tuple[type | Callable[..., Any], ...] = (()),
+) -> Callable[[_T], _T]:
+    return lambda a: a
+
+
+@__dataclass_transform__(field_specifiers=(Field, field))
+@overload
+def guiclass(cls: T) -> T:
+    ...
+
+
+@__dataclass_transform__(field_specifiers=(Field, field))
+@overload
+def guiclass(
+    *,
+    gui_name: str = "gui",
+    events_namespace: str = "events",
+    follow_changes: bool = True,
+    **dataclass_kwargs: Any,
+) -> Callable[[T], T]:
+    ...
+
+
+def guiclass(
+    cls: Optional[T] = None,
+    *,
+    gui_name: str = "gui",
+    events_namespace: str = "events",
+    follow_changes: bool = True,
+    **dataclass_kwargs: Any,
+) -> Union[T, Callable[[T], T]]:
+    """Turn class into a dataclass with a property (`gui_name`) that returns a gui.
+
+    This decorator is similar to `dataclasses.dataclass`, but it will also add an
+    `events` attribute to the class that is an instance of `psygnal.SignalGroup` (with a
+    signal for each field in the dataclass; see
+    https://psygnal.readthedocs.io/en/latest/dataclasses/ for details), and a `gui`
+    property that returns a `magicgui` widget, bound to the values of the dataclass
+    instance.
+
+    !!! note
+        This decorator is compatible with dataclasses using `slots=True`, however,
+        there is a potential for a memory leak that the user should be aware of.
+        If you create a `guiclass` instance, and then store a reference to its `gui`,
+        and then delete the instance, the `gui` will still be bound to the instance,
+        preventing it from being garbage collected.  To avoid this, you can call
+        `unbind_gui_from_instance(gui, instance)` before deleting the instance.
+
+    Parameters
+    ----------
+    cls : type
+        The class to turn into a dataclass.
+    gui_name : str, optional
+        The name of the property that will return a `magicgui` widget, by default "gui"
+    events_namespace : str, optional
+        The name of the attribute that will be added to the class, by default "events".
+        This attribute will be an instance of `psygnal.SignalGroup` that will be used
+        to connect events to the class.
+    follow_changes : bool, optional
+        If True, changes to the dataclass instance will be reflected in the gui, and
+        changes to the gui will be reflected in the dataclass instance, by default True
+    dataclass_kwargs : dict, optional
+        Additional keyword arguments to pass to `dataclasses.dataclass`.
+
+    Returns
+    -------
+    type
+        The dataclass.
+
+    Examples
+    --------
+    >>> @guiclass
+    ... class MyData:
+    ...     x: int = 0
+    ...     y: str = 'hi'
+    ...
+    ...     @button
+    ...     def reset(self):
+    ...         self.x = 0
+    ...         self.y = 'hi'
+    ...
+    >>> data = MyData()
+    >>> data.gui.show()
+    """
+
+    def _deco(cls: T) -> T:
+        if dataclass_kwargs.get("frozen", False):
+            raise ValueError(
+                "The 'guiclass' decorator does not support dataclasses with "
+                "`frozen=True`. If you need this feature, please open an issue at "
+                "https://github.com/pyapp-kit/magicgui/issues."
+            )
+
+        setattr(cls, gui_name, GuiBuilder(gui_name, follow_changes=follow_changes))
+
+        if not is_dataclass(cls):
+            cls = dataclass(cls, **dataclass_kwargs)  # type: ignore
+        cls = evented(cls, events_namespace=events_namespace)
+
+        setattr(cls, _GUICLASS_FLAG, True)
+        return cls
+
+    return _deco(cls) if cls is not None else _deco
+
+
+def is_guiclass(obj: object) -> TypeGuard[GuiClassProtocol]:
+    """Return `True` if obj is a guiclass or an instance of a guiclass."""
+    return is_dataclass(obj) and hasattr(obj, _GUICLASS_FLAG)
+
+
+@overload
+def button(func: F) -> F:
+    ...
+
+
+@overload
+def button(**kwargs: Any) -> Callable[[F], F]:
+    ...
+
+
+def button(
+    func: Optional[F] = None, **button_kwargs: Any
+) -> Union[F, Callable[[F], F]]:
+    """Add a method as a button to a `guiclass`, which calls the decorated method.
+
+    Parameters
+    ----------
+    func : callable
+        The method to decorate.  If None, returns a decorator that can be applied to a
+        method.
+    button_kwargs : dict, optional
+        Additional keyword arguments to pass to `magicgui.widgets.PushButton`.
+    """
+
+    def _deco(func: F) -> F:
+        button_kwargs.setdefault("name", func.__name__)
+        setattr(func, _BUTTON_ATTR, button_kwargs)
+        return func
+
+    return _deco(func) if func else _deco
+
+
+class GuiBuilder:
+    """Descriptor that builds a widget for a dataclass or instance."""
+
+    def __init__(self, name: str = "", follow_changes: bool = True):
+        self._name = name
+        self._follow_changes = follow_changes
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner):
+        wdg = build_widget(owner if instance is None else instance)
+
+        # look for @button-decorated methods
+        # TODO: move inside build_widget?
+        for k, v in vars(owner).items():
+            if hasattr(v, _BUTTON_ATTR):
+                kwargs = getattr(v, _BUTTON_ATTR)
+                button = PushButton(**kwargs)
+                if instance is not None:
+                    # call the bound method if we're in an instance
+                    button.clicked.connect(getattr(instance, k))
+                else:
+                    # TODO: what to do here?
+                    # probably doesn't make sense to call the method on the class
+                    ...
+                # TODO: allow inserting in a specific location
+                wdg.append(button)
+
+        if instance is not None and self._name:
+            if self._follow_changes:
+                bind_gui_to_instance(wdg, instance)
+            # cache it on the instance
+            # call del instance.<_name> to remove it
+            # this may fail in cases of __slots__ other attribute restrictions
+            # the cost is that the widget will be rebuilt on each access
+            with contextlib.suppress(AttributeError):
+                setattr(instance, self._name, wdg)
+        return wdg
+
+
+def bind_gui_to_instance(
+    gui: ContainerWidget, instance: Any, two_way: bool = True
+) -> None:
+    """Set change events in `gui` to update the corresponding attributes in `model`.
+
+    Parameters
+    ----------
+    gui : ContainerWidget
+        The widget to bind to the instance.
+    instance : Any
+        The instance to bind to the widget.  In most cases, this will be an instance
+        of a `guiclass`.
+    two_way : bool, optional
+        If True, changes to the instance will be reflected in the gui, by default True
+    """
+    events = getattr(instance, "events", None) if two_way else None
+    signals = events.signals if isinstance(events, SignalGroup) else {}
+
+    # in cases of classes with `__slots__`, widget.changed.connect_setattr(instance...)
+    # will show a RuntimeWarning because `instance` will not be weak referenceable.
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+
+        for widget in gui:
+            # we skip PushButton here, otherwise we will set the value of the
+            # button (a boolean) to some attribute on the instance, which is probably
+            # not what we want.
+            if isinstance(widget, ValueWidget) and not isinstance(widget, PushButton):
+                # connect changes in the widget to the instance
+                if hasattr(instance, widget.name):
+                    try:
+                        widget.changed.connect_setattr(instance, widget.name)
+                    except TypeError:
+                        warnings.warn(
+                            f"Could not bind {widget.name} to {instance}. "
+                            "This may be because the instance has __slots__ or "
+                            "other attribute restrictions. Please update psygnal."
+                        )
+
+                # connect changes from the instance to the widget
+                if widget.name in signals:
+                    signals[widget.name].connect_setattr(widget, "value")
+
+
+def unbind_gui_from_instance(gui: ContainerWidget, instance: Any) -> None:
+    """Unbind a gui from an instance.
+
+    This will disconnect all events that were connected by `bind_gui_to_instance`.
+    In cases where `guiclass` is used on a dataclass with `slots=True`, when a reference
+    to the gui is stored elsewhere, this must be called by the user before the instance
+    is deleted.
+
+    Parameters
+    ----------
+    gui : ContainerWidget
+        The widget bound to `instance`.
+    instance : Any
+        An instance of a `guiclass`.
+    """
+    for widget in gui:
+        if isinstance(widget, ValueWidget):
+            widget.changed.disconnect_setattr(instance, widget.name, missing_ok=True)
+
+
+# Class-based form ... which provides subclassing and inheritance (unlike @guiclass)
+
+
+@__dataclass_transform__(field_specifiers=(Field, field))
+class GuiClassMeta(type):
+    def __new__(cls, name, bases, attrs):
+        attrs[_GUICLASS_FLAG] = True
+        obj = type.__new__(cls, name, bases, attrs)
+        return evented(dataclass(obj))  # type: ignore
+
+
+# evented will warn "No mutable fields found in class <class '__main__.GuiClass'>"
+# no events will be emitted... but it will work fine for subclasses.
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+
+    class GuiClass(metaclass=GuiClassMeta):
+        gui = GuiBuilder()
+        if TYPE_CHECKING:
+            events: ClassVar[SignalGroup]
+
+            # the mypy dataclass magic doesn't work without the literal decorator
+            # it WILL work with pyright due to the __dataclass_transform__ above
+            # here we just avoid a false error in mypy
+            def __init__(self, *args, **kwargs) -> None:
+                ...
---
+++ tests/test_gui_class.py
@@ -0,0 +1,149 @@
+import contextlib
+import sys
+from dataclasses import asdict, dataclass
+from typing import TYPE_CHECKING, ClassVar
+from unittest.mock import Mock
+
+import psygnal
+import pytest
+
+from magicgui.schema._guiclass import (
+    GuiClass,
+    button,
+    guiclass,
+    is_guiclass,
+    unbind_gui_from_instance,
+)
+from magicgui.widgets import Container, PushButton
+
+
+def test_guiclass():
+    """Test that the guiclass decorator works as expected."""
+    mock = Mock()
+
+    @guiclass
+    class Foo:
+        a: int = 1
+        b: str = "bar"
+
+        @button
+        def func(self):
+            mock(asdict(self))
+
+        # example recommended for type checking
+        if TYPE_CHECKING:
+            gui: ClassVar[Container]
+            events: ClassVar[psygnal.SignalGroup]
+
+    foo = Foo()
+
+    assert foo.a == 1
+    assert foo.b == "bar"
+
+    assert isinstance(foo.gui, Container)
+    assert isinstance(foo.gui.func, PushButton)
+    assert foo.gui.a.value == 1
+    assert foo.gui.b.value == "bar"
+
+    foo.gui.a.value = 3
+    assert foo.a == 3
+
+    foo.b = "baz"
+    assert foo.gui.b.value == "baz"
+
+    foo.func()
+    mock.assert_called_once_with({"a": 3, "b": "baz"})
+    assert is_guiclass(Foo)
+    assert is_guiclass(foo)
+
+
+def test_frozen_guiclass():
+    """Test that the guiclass decorator works as expected."""
+
+    with pytest.raises(ValueError, match="not support dataclasses with `frozen=True`"):
+
+        @guiclass(frozen=True)
+        class Foo:
+            a: int = 1
+            b: str = "bar"
+
+
+def test_on_existing_dataclass():
+    """Test that the guiclass decorator works on pre-existing dataclasses."""
+
+    @guiclass
+    @dataclass
+    class Foo:
+        a: int = 1
+        b: str = "bar"
+
+    foo = Foo()
+    assert foo.a == 1
+    assert foo.b == "bar"
+    assert isinstance(foo.gui, Container)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="slots are python3.10 or higher")
+def test_slots_guiclass():
+    """Test that the guiclass decorator works as expected."""
+
+    psyg_version = tuple(int(x) for x in psygnal.__version__.split(".")[:3])
+    old_psygnal = psyg_version < (0, 6, 1)
+
+    @guiclass(slots=True)
+    class Foo:
+        a: int = 1
+        b: str = "bar"
+
+    foo = Foo()
+
+    with (
+        pytest.warns(UserWarning, match="Please update psygnal")
+        if old_psygnal
+        else contextlib.nullcontext()
+    ):
+        gui = foo.gui
+        # note that with slots=True, the gui is recreated on every access
+        assert foo.gui is not gui
+
+    assert isinstance(gui, Container)
+    assert gui.a.value == 1
+    foo.b = "baz"
+    assert gui.b.value == "baz"
+    gui.a.value = 3
+
+    if old_psygnal:
+        # no change :(
+        assert foo.a == 1
+        assert len(gui.a.changed._slots) == 2
+    else:
+        assert foo.a == 3
+        assert len(gui.a.changed._slots) == 3
+
+    unbind_gui_from_instance(gui, foo)
+    assert len(gui.a.changed._slots) == 2
+    del foo
+
+
+def test_guiclass_as_class():
+
+    # variant on @guiclass, using class instead of decorator
+    class T2(GuiClass):
+        x: int
+        y: str = "hi"
+
+        @button
+        def foo(self):
+            return asdict(self)
+
+    t2 = T2(1)
+    assert t2.x == 1
+    assert t2.y == "hi"
+    assert t2.gui.x.value == 1
+    assert t2.gui.y.value == "hi"
+    t2.gui.x.value = 3
+    assert t2.x == 3
+    t2.y = "baz"
+    assert t2.gui.y.value == "baz"
+    assert isinstance(t2.gui.foo, PushButton)
+    assert t2.foo() == {"x": 3, "y": "baz"}
