3b1d373bb6f27ffe9c1385e2605bc4f534f4bd7f
Talley Lambert <talley.lambert@gmail.com>
2022-12-12 10:54:29 -0500

    feat: add experimental namespace (#526)

---
+++ src/magicgui/experimental.py
@@ -0,0 +1,8 @@
+"""Experimental features.
+
+Note: these are not guaranteed to be stable. Names and parameters may change or be
+removed in future releases. Use at your own risk.
+"""
+from .schema._guiclass import button, guiclass, is_guiclass
+
+__all__ = ["guiclass", "button", "is_guiclass"]
