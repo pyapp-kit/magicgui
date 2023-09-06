2fd984399a66e82c159a2d6af71672a0b74d9167
Talley Lambert <talley.lambert@gmail.com>
2023-09-05 20:14:00 -0400

    fix example tests

--- docs/examples/demo_widgets/image.py
+++ docs/examples/demo_widgets/image.py
@@ -5,9 +5,11 @@ Example of creating an Image Widget from a file.
 (This requires pillow, or that magicgui was installed as ``magicgui[image]``)
 """
 
+from pathlib import Path
+
 from magicgui.widgets import Image
 
-img = "../../images/_test.jpg"
-image = Image(value=img)
+img = Path(__file__).parent.parent.parent / "images" / "_test.jpg"
+image = Image(value=str(img))
 image.scale_widget_to_image_size()
 image.show(run=True)
