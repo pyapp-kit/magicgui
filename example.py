dc008d6d41b7d20e7d7279edf9602b73cccdaa75
Talley Lambert <talley.lambert@gmail.com>
2023-10-22 16:03:24 -0400

    update example

--- example.py
+++ example.py
@@ -1,30 +1,28 @@
 from magicgui import widgets
 
-dw = widgets.PushButton(text="Hello World!")
-tb = widgets.ToolBar()
-tb.add_button(text="Hello", icon="mdi:folder")
-tb.add_spacer()
-tb.add_button(text="World!", icon="mdi:square-edit-outline")
-
-
 main = widgets.MainWindow()
-main.add_dock_widget(dw)
+
+# toolbar
+tb = widgets.ToolBar()
+tb.add_button(text="Folder", icon="mdi:folder")
+tb.add_spacer()
+tb.add_button(text="Edit", icon="mdi:square-edit-outline")
 main.add_tool_bar(tb)
 
-# sb = widgets.StatusBar()
-# sb.set_message("Hello Status!")
-# main.set_status_bar(sb)
-# or
-main.status_bar.set_message("")
+# status bar
+main.status_bar.set_message("Hello Status!", timeout=5000)
 
+# doc widgets
+main.add_dock_widget(widgets.PushButton(text="Push me."), area="right")
+
+# menus
 file_menu = main.menu_bar.add_menu("File")
 assert file_menu is main.menu_bar["File"]  # can also access like this
 file_menu.add_action("Open", callback=lambda: print("Open"))
-
 subm = file_menu.add_menu("Submenu")
 subm.add_action("Subaction", callback=lambda: print("Subaction"))
 subm.add_separator()
 subm.add_action("Subaction2", callback=lambda: print("Subaction2"))
 
-main.width = 800
+main.height = 400
 main.show(run=True)
