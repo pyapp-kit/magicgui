0f080b215e0144766865c827ebe0068cc2ea9c9f
James Evans <j5evans@student.bridgew.edu>
2024-03-26 13:13:51 -0400

    Added a simple example of a calculation that takes some time to try the alert box on calculation completion.

--- MagicGuiTesting.py
+++ MagicGuiTesting.py
@@ -22,5 +22,17 @@ def snells_law(aoi=30.0, n1=Medium.Glass, n2=Medium.Water, degrees=True):
         return "Total internal reflection!"
 
 
+@magicgui(call_button="calculate", result_widget=True)
+def long_calculation_test():
+    summed = 0
+    for _ in range(0, 10):
+        for _ in range(0, 3000):
+            for x in range(0, 10000):
+                summed += x
+
+    return summed
+
+
 # your function is now capable of showing a GUI
-snells_law.show(run=True)
+# snells_law.show(run=True)
+long_calculation_test.show(run=True)
