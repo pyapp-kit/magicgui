f87b4a59c80e29ad21400f4a8c14e345868185da
“Graham <graham.inzana@gmail.com>
2024-04-17 11:13:04 -0400

    made a function to return window title

--- src/magicgui/backends/_qtpy/widgets.py
+++ src/magicgui/backends/_qtpy/widgets.py
@@ -544,6 +544,9 @@ class Container(
     def _mgui_get_root_native_widget(self):
         return self._qwidget
 
+    def _mgui_get_window_title(self):
+        return self._qwidget.windowTitle()
+
     def _mgui_get_native_widget(self):
         # Return widget with the layout set
         return self._qwidget.widget() if self._is_scrollable else self._qwidget
