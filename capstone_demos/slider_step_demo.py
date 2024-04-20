803a1b5b55cccfe5aff3904c78b10c46c317b891
jaredjplante <jaredjplante@gmail.com>
2024-04-20 19:29:47 -0400

    created slider_step_demo.py

---
+++ capstone_demos/slider_step_demo.py
@@ -0,0 +1,25 @@
+from magicgui import magicgui
+
+
+@magicgui(auto_call=True, slider_int={"label" : "val = 500", "widget_type": "Slider", "value": 500, "min": 400, "max": 1000, "step": 100,},)
+def step_demo(slider_int=500,):
+    step_demo.slider_int.label = "val = " + str(slider_int)
+    return slider_int
+
+
+
+@magicgui(auto_call=True, slider_int={"label" : "val = 500", "widget_type": "Slider", "value": 500, "min": 400, "max": 1000, "step": 100,},)
+def step_workaround(slider_int=500,):
+    step = step_workaround.slider_int.step
+    rem = slider_int % step
+    if rem > step_workaround.slider_int.step/2 :
+        slider_int += step - rem
+    else:
+        slider_int -= rem
+    step_workaround.slider_int.value = slider_int
+    step_workaround.slider_int.label = "val = " + str(slider_int)
+    return slider_int
+
+
+#step_demo.show(run=True)
+step_workaround.show(run=True)
