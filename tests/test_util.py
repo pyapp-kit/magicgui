2cf83ede6e8ce01ee0c1884e3cb69dfdaad6a8e0
Grzegorz Bokota <bokota+github@gmail.com>
2024-08-06 22:36:27 +0200

    fix: Fix checking sublcasses of Future (#665)
    * fix: Fix checking sublcasses of Future
    
    * skip test on python 3.8
    
    * add negative test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,7 @@
 import sys
 import typing
 from collections.abc import Mapping, Sequence
+from concurrent.futures import Future
 
 import pytest
 
@@ -78,3 +79,10 @@ class TestSafeIsSubclass:
         assert safe_issubclass(typing.Tuple[int, int], typing.Tuple[int, ...])
         assert not safe_issubclass(typing.Tuple[int, int], typing.Tuple[int, str])
         assert not safe_issubclass(typing.Tuple[int, int], typing.Tuple[int, int, int])
+
+    @pytest.mark.skipif(sys.version_info < (3, 9), reason="Future[] is supported in 3.9+")
+    def test_subclass_future(self):
+        assert safe_issubclass(Future[typing.List[int]], Future[list[int]])
+        assert safe_issubclass(Future[typing.List[int]], Future[list])
+        assert safe_issubclass(Future[list[int]], Future[typing.List[int]])
+        assert not safe_issubclass(Future[list[int]], Future[typing.List[str]])
