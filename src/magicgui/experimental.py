f5e89e569ff51233ed3ef56f8d92d9cd5fc380dc
Talley Lambert <talley.lambert@gmail.com>
2025-02-23 13:06:23 -0500

    fix: fix name collisions between dataclass fields and guiclass container attributes (#688)
    * fix: fix name collisions on guiclass dataclass
    
    * fix test
    
    * lint
    
    * bump minimum version
    
    * undo private name change because magicclass is accessing it
    
    * use first not last
    
    * remove uvlock

--- src/magicgui/experimental.py
+++ src/magicgui/experimental.py
@@ -4,6 +4,6 @@ Note: these are not guaranteed to be stable. Names and parameters may change or 
 removed in future releases. Use at your own risk.
 """
 
-from .schema._guiclass import button, guiclass, is_guiclass
+from .schema._guiclass import GuiBuilder, button, guiclass, is_guiclass
 
-__all__ = ["button", "guiclass", "is_guiclass"]
+__all__ = ["GuiBuilder", "button", "guiclass", "is_guiclass"]
