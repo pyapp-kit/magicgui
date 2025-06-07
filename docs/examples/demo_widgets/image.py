fb4b76560b2f63b0ee930f9b7f2d2ca558307337
Talley Lambert <talley.lambert@gmail.com>
2025-06-07 12:37:50 -0400

    mkdocs-gallery hack

--- docs/examples/demo_widgets/image.py
+++ docs/examples/demo_widgets/image.py
@@ -9,7 +9,11 @@ from pathlib import Path
 
 from magicgui.widgets import Image
 
-test_jpg = Path(__file__).parent.parent.parent / "images" / "_test.jpg"
+try:
+    test_jpg = Path(__file__).parent.parent.parent / "images" / "_test.jpg"
+except NameError: # hack to support mkdocs-gallery build, which doesn't define __file__
+    test_jpg = "../../images/_test.jpg"
+
 image = Image(value=test_jpg)
 image.scale_widget_to_image_size()
 image.show(run=True)
